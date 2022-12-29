c8e6c172330115feae7e199bed865d4e0c58169f
stpack <sattha.prat@hotmail.com>
2022-12-29 16:04:37 +0700

    Intitailize Project Use vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from 'react'
+import picc from '@/assets/pic_react.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div className="text-red border">tzxt</div>
+    <div className='App'>
+      <div className='text-red border'>tzxt</div>
+      <img src={picc} alt='' />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
